24f899c7730b2d7ddf201d5eeb35d846b28cba3e
Mika <Mika.Skjelnes.6139@student.uu.se>
2021-01-27 21:09:48 +0200

    Generated index
    First version of index generated from script.

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,14 @@
 function qTest(){
     console.log("Hi Reality");
 }
+
+function inspect(num){
+    console.log("hi");
+    tag = document.getElementById("inspect"+num);
+    if(tag.style.opacity == 1.0){
+        tag.style.opacity = 0.0;
+    }
+    else{
+    tag.style.opacity = 1.0;
+    }
+}
